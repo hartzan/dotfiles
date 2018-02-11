1ab0446a207a4d5ccba113cf4476edfea0062560
Harri Lahtinen <lahtinen.harri@gmail.com>
2018-02-11 11:54:40 +0200

    top & bottom bar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,30 +1,18 @@
 #!/usr/bin/env sh
 
-# Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar1 and bar2
-polybar example &
-
-
-echo "Bars launched..."
-
-#!/bin/bash
-#!/usr/bin/env sh
+polybar_proc=$(pgrep -u $UID -x polybar)
 
 # Terminate already running bar instances
-#killall -q polybar
+for i in ${polybar_proc} ; do
+    kill -9 $i
+done
 
-# Wait until the processes have been shut down
-#while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-#polybar example &
+# Launch bar top & bottom
+polybar top &
+polybar bottom &
 
-#for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -c ~/.config/polybar/config & done
-#feh --bg-scale ~/.config/wall.png
-
-#echo "Bars launched..."
+echo "Bars launched..."
